16b7f4e0ad2615c5029315f4bd151c075da9e78e
Manas <devmanas45@gmail.com>
2024-09-03 07:52:21 +0530

    Add length range and checkboxes

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -25,6 +25,27 @@ function App() {
         className='outline-none w-full py-1 px-3 rounded-lg' placeholder='password' readOnly />
         <button className='bg-blue-500 px-3 py-3 text-white rounded-xl ml-2'>Copy</button>
       </div>
+      <div className="flex text-sm gap-x-2">
+
+        <div className='flex items-center gag-x-1'>
+          <input type="range" min={8} max={100} value={length} className="cursor-pointer" onChange={(e)=>{setLength(e.target.value)}}/>
+          <label className="px-5 text-lg">Length: {length}</label>
+
+        
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input type="checkbox" defaultChecked={numberAllowed} id="numberInput" onChange={()=>{
+            setNumberAllowed((prev)=>!prev);
+  
+          }} />
+          <label htmlFor="numberInput">Number</label>
+          <input type="checkbox" defaultChecked={charAllowed} id="charInput" onChange={()=>{
+            setCharAllowed((prev)=>!prev);
+  
+          }} />
+          <label htmlFor="numberInput">Character</label>
+        </div>
+      </div>
     </div>
     
       
