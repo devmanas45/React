003964bb8413e57d5b460e97e0fef45820935d92
Manas <devmanas45@gmail.com>
2024-09-03 16:11:33 +0530

    Add useRef hook and useEffect hook to complete

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,29 +1,41 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 function App() {
   const [length,setLength]=useState(8);
   const [numberAllowed,setNumberAllowed]=useState(false);
   const [charAllowed,setCharAllowed]=useState(true);
-  const [password,setPassword]=useState("")
+  const [password,setPassword]=useState("");
+  const passwordRef=useRef(null);
+
   const passwordGenerator=useCallback(()=>{
     let pass="";
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if(numberAllowed) str+="0123456789";
     if(charAllowed) str+="!@#$%^&*(){}[]+=-";
-    for(let i=1;i<=Array.length;i++){
+    for(let i=1;i<=length;i++){
       let char=Math.floor(Math.random()*str.length+1);
-      pass=str.charAt(char);
+      pass+=str.charAt(char);
     }
     setPassword(pass);
 
-  },[length,numberAllowed,charAllowed,setPassword])
+  },[length,numberAllowed,charAllowed,setPassword]);
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed]);
+  const copyPassword=useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,99);
+    window.navigator.clipboard.writeText(password);
+
+  },[password]
+  );
   return (
     <>
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 my-8 text-orange-500 bg-gray-700">
       <h1 className="text-white text-4xl my-4">Password generator</h1>
       <div className="flex-shadow rounded-lg overflow-hidden mb-4 flex" >
-        <input type="text" value={password} 
+        <input type="text" value={password}  ref={passwordRef}
         className='outline-none w-full py-1 px-3 rounded-lg' placeholder='password' readOnly />
-        <button className='bg-blue-500 px-3 py-3 text-white rounded-xl ml-2'>Copy</button>
+        <button className='bg-blue-500 px-3 py-3 text-white rounded-xl ml-2' onClick={copyPassword}>Copy</button>
       </div>
       <div className="flex text-sm gap-x-2">
 
@@ -47,8 +59,6 @@ function App() {
         </div>
       </div>
     </div>
-    
-      
     </>
   )
 }
